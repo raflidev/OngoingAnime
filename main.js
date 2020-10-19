3da95a0cd46ad52ef5313ed6f247408398644209
Rafli Ramadhan <rafliramdhn@gmail.com>
2020-10-20 00:31:21 +0700

    add episode section

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { getUrl } from "./scrapping";
+import { getUrl, getListEpisode } from "./scrapping";
 import express from "express";
 import CryptoJS from "crypto-js";
 
@@ -6,13 +6,13 @@ const app = express();
 const port = 3000;
 app.set("view engine", "ejs");
 
-var textString = "rafli"; // Utf8-encoded string
-var words = CryptoJS.enc.Utf8.parse(textString); // WordArray object
-var base64 = CryptoJS.enc.Base64.stringify(words); // string: 'SGVsbG8gd29ybGQ='
-console.log(base64);
+// var textString = "rafli"; // Utf8-encoded string
+// var words = CryptoJS.enc.Utf8.parse(textString); // WordArray object
+// var base64 = CryptoJS.enc.Base64.stringify(words); // string: 'SGVsbG8gd29ybGQ='
+// console.log(base64);
 
-var text = CryptoJS.enc.Utf8.stringify(words); // 'Hello world'
-console.log(text);
+// var text = CryptoJS.enc.Utf8.stringify(words); // 'Hello world'
+// console.log(text);
 async function main() {
   const result = await getUrl();
   return result;
@@ -29,8 +29,13 @@ app.get("/home", async (req, res) => {
   });
 });
 
-app.get("/episode/:base", (req, res) => {
-  res.send(req.params.base);
+app.get("/episode/:base", async (req, res) => {
+  const tampilEpisode = await getListEpisode(
+    CryptoJS.enc.Utf8.stringify(CryptoJS.enc.Base64.parse(req.params.base))
+  );
+  res.render("episode", {
+    episode: tampilEpisode,
+  });
 });
 
 app.listen(port, () => {
--- scrapping.js
+++ scrapping.js
@@ -2,26 +2,43 @@ import cheerio from "cheerio";
 import axios from "axios";
 import CryptoJS from "crypto-js";
 
-const anime = [];
+var anime = [];
 export async function getUrl() {
-  if (anime.length == 0) {
-    const link = "https://otakudesu.tv/ongoing-anime/";
-    const result = await axios.get(link);
-    const $ = cheerio.load(result.data);
-    anime.push({
-      date: Date.now(),
-      ongoing: [],
+  anime = [];
+  const link = "https://otakudesu.tv/ongoing-anime/";
+  const result = await axios.get(link);
+  const $ = cheerio.load(result.data);
+  anime.push({
+    date: Date.now(),
+    ongoing: [],
+  });
+  $(".detpost").each(function () {
+    anime[0].ongoing.push({
+      judul: $(this).find(".jdlflm").html(),
+      tanggal: $(this).find(".newnime").html(),
+      link: $(this).find("a").attr("href"),
+      base: CryptoJS.enc.Base64.stringify(
+        CryptoJS.enc.Utf8.parse($(this).find("a").attr("href"))
+      ),
     });
-    $(".detpost").each(function () {
-      anime[0].ongoing.push({
-        judul: $(this).find(".jdlflm").html(),
-        tanggal: $(this).find(".newnime").html(),
-        link: $(this).find("a").attr("href"),
-        base: CryptoJS.enc.Base64.stringify(
-          CryptoJS.enc.Utf8.parse($(this).find("a").attr("href"))
-        ),
-      });
-    });
-  }
+  });
   return anime;
+}
+
+var episode = [];
+export async function getListEpisode(url) {
+  const result = await axios.get(url);
+  const $ = cheerio.load(result.data);
+  episode = [];
+  $(".episodelist li").each(function () {
+    if ($(this).find("a[data-wpel-link=internal]").attr("href") != undefined) {
+      episode.push({
+        link: $(this).find("a[data-wpel-link=internal]").attr("href"),
+        judul: $(this).find("a[data-wpel-link=internal]").text(),
+        tanggal: $(this).find(".zeebr").text(),
+      });
+    }
+  });
+  console.log(episode.length);
+  return episode.reverse();
 }
